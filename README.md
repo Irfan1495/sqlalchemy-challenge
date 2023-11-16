d51cbb4695860cee4f8cfeb8b95fdf46d062d9e6
Irfan1495 <144286844+Irfan1495@users.noreply.github.com>
2023-11-15 22:59:31 -0500

    Final Changes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # sqlalchemy-challenge
+
+In this week's challenge I went through different CSV files and Sqlite file to analyize it
+
+The Assignment was consistent of two parts.
+
+Analyze and Explore the Climate Data:
+    In this section I created and worked on an ipnb file. I analyize the climate csv files and worked on some graphs as well.
+
+Design your Climate App:
+    In this section I designed a Flask API based on the queries that I developed in the previous section.
+
+Connectivity with GitHub:
+    My respository is connected to my local repository through GitHub App on my computer. Once I made the repository on GitHub I simply connected it to my local repository through my GitHub desktop App on my computer. As I worked on the Challege I would push the changes through the Desktop GitHub App.
+
+Help: I got help from the class notes. I also searched YouTube and Google. I would say most of the help came from the lectures and activities and then some from YouTube and Google.
+    I also colloborated with two of my classmates; Anthony and Priyanka.
+    
+    
+
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,105 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+
+@app.route("/api/v1.0/names")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all precipitation"""
+    # Query all precipitation
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # Convert list into normal list
+    precipitation = list(np.ravel(results))
+
+    return jsonify(precipitation)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of stations"""
+    # Query all stations
+    results = session.query(stations.name).all()
+    
+    all_stations = list(np.ravel(results))
+    
+    return jsonify(all_stations)
+
+
+@app.route("/api/v1.0/stations")
+def temperature():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of temperatures"""
+    # Query all temperatures
+    results = session.query(Measurement.tobs, Measurement.date).all()
+    
+    all_temperature = list(np.ravel(results))
+    
+    return jsonify(all_temperature)
+
+    session.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+    
+    
+    
